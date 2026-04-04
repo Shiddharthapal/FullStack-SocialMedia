47b7692d8108b5abfefe3030f93990070103b81e
Shiddhartha Pal <shiddhartpal01355@gmail.com>
2026-04-04 11:29:21 +0600

    in layout add some css file and create home page and link the page with app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../redux/store";
 import Layout from "../layouts/Layout";
 import ProtectedRoute from "./ProtectedRoute";
 import Login from "./pages/login/index";
+import Home from "./pages/home/index";
 // import Register from "./pages/register/index";
 
 
@@ -17,6 +18,7 @@ export default function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
               {/* <Route path="/createaccount" element={<Register />} /> */}
               {/* <Route
                 path="/"
